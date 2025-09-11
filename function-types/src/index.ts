d80e1b7dd766b85863dbc8d23388eaa6fa77cf1c
Guilherme <guilhermefrance9@gmail.com>
2025-09-11 16:03:07 -0300

    first commit

--- function-types/src/index.ts
+++ function-types/src/index.ts
@@ -1,68 +1,30 @@
+// function types
 
-//interfaces 
+type Args = string | number | boolean;
+function bootstrap(dirname: string, args?: Args[]): boolean{
+    return true;
+}
 
-declare global{
-    interface Console{
-        sayHello() : void;
+type MainFunction = (args: string[]) => void;
+
+const main: MainFunction = (args) => {
+
+}
+
+interface Functions {
+    run(context: any): void;
+    execute(): boolean;
+    handle(req:Request, res: Response):void;
+}
+
+const funcs: Functions = {
+    execute() {
+        return true;
+    },
+    run(){
+
+    },
+    handle(req, res) {
+        
     }
 }
-
-Object.assign( console, {
-    sayHello(){
-        console.log("Hello")
-    }}
-)
-
-console.sayHello();
-
-
- interface UserWallet {
-    coins?: number;
-    credits?: number;
-}
-
-interface User {
-    name: string;
-    createdAt: Date;
-    wallet?:UserWallet
-    }
-
-function createUser(name: string): User{
-    return{name, createdAt: new Date()} 
-}
-
-function updateWallet(user: User, wallet: UserWallet){
-    user.wallet = { ...user.wallet, ...wallet }
-
-}
-
-
-const Guilherme = createUser("Guilherme");
-
-
-interface Admin extends User{
-    ban(user: User): void;
-    kick(user: User): void;
-}    
-
-function promoteUser(user: User): Admin{
-    return {
-        ...user,
-        ban(userToBan){
-            console.log(userToBan, "foi banido por", this.name)
-        },
-        kick(userToKick){
-            console.log(userToKick, "foi kickado por", this.name)
-        }}}
-
-
-
-const adminGuilherme = promoteUser(Guilherme);
-
-function adminAction(admin: Admin){
-
-}
-
-adminAction(adminGuilherme)
-
-updateWallet(Guilherme, { coins : 10}) 
